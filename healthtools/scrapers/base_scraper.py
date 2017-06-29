cb4ba3b37d91017a7e2fbb4d333d605dc27ccf53
omorilewa <mariam3105@gmail.com>
2017-06-30 00:19:29 +0100

    fix errs

--- healthtools/scrapers/base_scraper.py
+++ healthtools/scrapers/base_scraper.py
@@ -66,8 +66,8 @@ class Scraper(object):
         print ""
         print "[{0}] ".format(re.sub(r"(\w)([A-Z])", r"\1 \2", type(self).__name__))
         print "[{0}] - Started Scraper.".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-
-        divisor = self.get_total_number_of_pages() / 5
+        self.get_total_number_of_pages()
+        divisor = self.num_pages_to_scrape / 5
         for page_num in range(1, self.num_pages_to_scrape + 1):
             if page_num == divisor:
                 print "[{}] - Scraped {}% out of {} pages.".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), (page_num/self.num_pages_to_scrape) * 100, self.num_pages_to_scrape)

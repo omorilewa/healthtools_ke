adabfa3cd270923fe51f093676efe8025cb7806e
omorilewa <mariam3105@gmail.com>
2017-06-29 19:53:48 +0100

    trys

--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     # Initialize the Scrapers
     # doctors_scraper = DoctorsScraper()
     # foreign_doctors_scraper = ForeignDoctorsScraper()
-    clinical_officers_scraper = ClinicalOfficersScraper()
+    # clinical_officers_scraper = ClinicalOfficersScraper()
     # healthfacilities_scraper = HealthFacilitiesScraper()
 
     # scraping you softly with these bots...
